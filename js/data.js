7881a2e6714a8e88682adbddd07d7e179c02c7ae
Samuel Castaneda <yugiohcoolguy@gmail.com>
2024-07-31 18:26:08 +0000

    I don't know what happen but this is me finishing storing values into an object and having my image reset every submit

--- js/data.js
+++ js/data.js
@@ -1,3 +1,4 @@
+'use strict';
 /* exported data
 
 const data = {
--- js/main.js
+++ js/main.js
@@ -1,18 +1,11 @@
-/* global  */
-
-interface FormElements extends HTMLFormControlsCollection {
-  photoUrl: HTMLInputElement;
-  title: HTMLInputElement;
-  notes: HTMLTextAreaElement;
-}
-
+'use strict';
+/* global data */
 const $image = document.getElementsByTagName('img');
-const imf = $image[0] as HTMLImageElement;
-const $newImg = document.getElementById('photo-url') as HTMLInputElement;
-const $form = document.querySelector('#form') as HTMLFormElement;
-
-function imgChanger(event: Event): void {
-  const $change = event.target as HTMLInputElement;
+const imf = $image[0];
+const $newImg = document.getElementById('photo-url');
+const $form = document.querySelector('#form');
+function imgChanger(event) {
+  const $change = event.target;
   const picture = $change.value;
   if (urlChecker(picture) === true) {
     imf.src = picture;
@@ -21,20 +14,17 @@ function imgChanger(event: Event): void {
       'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/403.png';
   }
 }
-
-function urlChecker(string: string): boolean {
+function urlChecker(string) {
   const res = string.match(
     /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g,
   );
   return res !== null;
 }
-
 if ($newImg) $newImg.addEventListener('change', imgChanger);
 if (!$newImg) throw new Error('The Image did not load.');
-
-$form.addEventListener('submit', (event: Event) => {
+$form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const $formElements = $form.elements as FormElements;
+  const $formElements = $form.elements;
   const $titleInput = $formElements.title.value;
   console.log($titleInput);
   const $photoInput = $formElements.photoUrl.value;
@@ -56,6 +46,7 @@ $form.addEventListener('submit', (event: Event) => {
     nextEntryId: entryId,
   };
   console.log(data);
+  writeData();
   entryId++;
   $form.reset();
   const photoReset = '/images/placeholder-image-square.jpg';
